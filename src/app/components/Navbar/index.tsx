14818cd320f773f0b795d523e5de1af1b6c5186e
Sangeet Parashar <sangeetparashar@gmail.com>
2024-08-30 15:40:34 -0400

    add dashboard endpoint and revamp the navbar

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -1,14 +1,34 @@
 import { NextPage } from "next";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import Brand from "@/app/components/Brand";
 
 import Wallet, { WalletProps } from "../Wallet";
 
 const Navbar: NextPage<WalletProps> = ({ setConnectModalOpen }) => {
+  const pathName = usePathname();
+
   return (
     <header className="fixed top-0 w-full z-40">
-      <nav className="flex items-center justify-between py-4 px-8 text-white">
-        <Brand />
+      <nav className="flex items-center justify-between pl-8 text-white bg-[#D9D9D9] h-[55px]">
+        <div className="flex items-center pl-2 text-black">
+          <Brand />
+          <span
+            className={`p-2 ${
+              pathName === "/dashboard" ? "border-b-2 border-black pb-2" : ""
+            }`}
+          >
+            <Link href="/dashboard">DASHBOARD</Link>
+          </span>
+          <span
+            className={`p-2 ${
+              pathName === "/stake/btc" ? "border-b-2 border-black pb-2" : ""
+            }`}
+          >
+            <Link href="/stake/btc">RESTAKE</Link>
+          </span>
+        </div>
         <Wallet setConnectModalOpen={setConnectModalOpen} />
       </nav>
     </header>
