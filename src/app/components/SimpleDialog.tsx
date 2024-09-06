401e3c10b970033f444a24b6af050d3037528ad5
desamtralized <sambarbosaa@gmail.com>
2024-09-06 16:31:09 -0500

    using default tailwind values in simpledialog

--- src/app/components/SimpleDialog.tsx
+++ src/app/components/SimpleDialog.tsx
@@ -25,20 +25,20 @@ const SimpleDialog: React.FC<SimpleDialogProps> = ({
         <Dialog.Overlay className="fixed inset-0 bg-black/50" />
         <Dialog.Content
           className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
-          border-2 border-[#DCD4C9] bg-yieldi-beige shadow-lg w-[90%] max-w-dialog text-[#332B29]"
+          border-2 border-yieldi-gray-200 bg-yieldi-beige shadow-lg w-[90%] max-w-dialog text-yieldi-brown"
         >
-          <div className="flex justify-between items-center mb-4 border-b border-[#DCD4C9]">
-            <Dialog.Title className="text-2xl font-bold text-[#332B29] p-2">
+          <div className="flex justify-between items-center mb-4 border-b border-yieldi-gray-200">
+            <Dialog.Title className="text-2xl font-bold p-2">
               {title}
             </Dialog.Title>
-            <Dialog.Close className="text-[#332B29] border-x border-[#DCD4C9] p-2">
+            <Dialog.Close className="border-x border-yieldi-gray-200 p-2">
               <Cross2Icon width={36} height={36} />
             </Dialog.Close>
           </div>
           <div className="mb-6 p-4">{message}</div>
           <div className="flex justify-center px-2 pb-3">
             <button
-              className="w-full py-4 px-2 bg-yieldi-green font-medium text-sm cursor-pointer border border-[#DCD4C9] uppercase"
+              className="w-full py-4 px-2 bg-yieldi-green font-medium text-sm cursor-pointer border border-yieldi-gray-200 uppercase"
               onClick={onButtonClick}
             >
               {buttonTitle}
