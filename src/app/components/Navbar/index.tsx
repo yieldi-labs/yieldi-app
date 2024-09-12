5ff7064ff163b6a155991b79165213cf85d92382
desamtralized <sambarbosaa@gmail.com>
2024-09-12 20:01:08 +0000

    yet another lint commit

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -20,7 +20,7 @@ const Navbar: NextPage<WalletProps> = ({ setConnectModalOpen }) => {
   return (
     <header className="fixed top-0 w-full z-40 border-b border-yieldi-gray-200 flex flex-col">
       <nav className="flex items-center justify-between md:pl-8 text-white bg-yieldi-beige h-[55px] flex-1">
-        <div className="flex items-center pl-2 text-black flex-shrink-0">
+        <div className="flex items-center pl-2 text-black shrink-0">
           <Brand />
           <div className="hidden md:flex">
             <div className="border-yieldi-gray-200 border-x">
@@ -47,21 +47,21 @@ const Navbar: NextPage<WalletProps> = ({ setConnectModalOpen }) => {
             </div>
           </div>
         </div>
-        <div className="flex md:justify-end justify-center flex-grow">
+        <div className="flex md:justify-end justify-center grow">
           <Wallet setConnectModalOpen={setConnectModalOpen} />
         </div>
         <button
-            className="flex flex-shrink-0 w-[56px] justify-center items-center gap-[10px] self-stretch"
-            onClick={toggleMenu}
-          >
-            <Image
-              src="/menu.svg"
-              alt="menu"
-              width={0}
-              height={0}
-              className="flex w-[38px] h-[41px] px-[4.75px] py-[10.25px] flex-col justify-center items-center shrink-0"
-            />
-        </button> 
+          className="flex shrink-0 w-[56px] justify-center items-center gap-[10px] self-stretch"
+          onClick={toggleMenu}
+        >
+          <Image
+            src="/menu.svg"
+            alt="menu"
+            width={0}
+            height={0}
+            className="flex w-[38px] h-[41px] px-[4.75px] py-[10.25px] flex-col justify-center items-center shrink-0"
+          />
+        </button>
       </nav>
       <div className={`${isMenuOpen ? "block" : "hidden"}`}>
         <div className="md:hidden">
